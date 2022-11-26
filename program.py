66a3c6bbe018f92c750a5680529c6451fbbd6070
qurbi4 <111078851+qurbi4@users.noreply.github.com>
2022-11-26 20:14:40 +0300

    Update program.py
    BEBRA

--- program.py
+++ program.py
@@ -1,7 +1,10 @@
 def main():
-    print('My first git program!')
+    print('My first git program')
     print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
 
 
 if __name__ == '__main__':
     main()
+   
